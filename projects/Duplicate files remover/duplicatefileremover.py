8f8c3f1b92e9c963b6bd910de14f2fd881d6b669
PriddyBoi <jsopriddy@onmail.com>
2024-01-01 18:59:47 -0600

    n

--- projects/Duplicate files remover/duplicatefileremover.py
+++ projects/Duplicate files remover/duplicatefileremover.py
@@ -1,5 +1,7 @@
 import hashlib
 import os
+# Remove the import statement for hashlib since it is already imported above
+# import hashlib
 
 # Returns the hash string of the given file name
 
@@ -23,7 +25,7 @@ if __name__ == "__main__":
 
     # List to store deleted files
     deletedFiles = []
-    filelist = [f for f in os.listdir() if os.path.isfile(f)]
+    filelist = [f for f in os.listdir('C:/Users/jacob/OneDrive/Documents') if os.path.isfile(f)]
     for f in filelist:
         key = hashFile(f)
         # If key already exists, it deletes the file
