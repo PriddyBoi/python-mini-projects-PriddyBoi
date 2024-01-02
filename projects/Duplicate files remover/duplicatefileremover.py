8ae09bebe4f32da5e5949eea9df41dd4e7a52c76
PriddyBoi <jsopriddy@onmail.com>
2024-01-01 19:48:12 -0600

    Refactor duplicate file removal logic and fix file path issue

--- projects/Duplicate files remover/duplicatefileremover.py
+++ projects/Duplicate files remover/duplicatefileremover.py
@@ -1,13 +1,7 @@
 import hashlib
 import os
-import pytest  # noqa: F401
-
-# Remove the import statement for hashlib since it is already imported above
-# import hashlib
 
 # Returns the hash string of the given file name
-
-
 def hashFile(filename):
     # For large files, if we read it all together it can lead to memory overflow, So we take a blocksize to read at a time
     BLOCKSIZE = 65536
@@ -27,18 +21,25 @@ if __name__ == "__main__":
 
     # List to store deleted files
     deletedFiles = []
-filelist = [f for f in os.listdir('C:/Users/jacob/Downloads') if os.path.isfile(os.path.join('C:/Users/jacob/Downloads', f))]
-for f in filelist:
-    key = hashFile(f)
-    # If key already exists, it deletes the file
-    if key in hashMap.keys():
-        deletedFiles.append(f)
-        os.remove(f)
+    
+    # Specify the directory path to search for duplicate files
+    directory = 'C:/Users/jacob/Downloads/test'
+    
+    # Generate the file list from the specified directory
+    filelist = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    
+    for f in filelist:
+        key = hashFile(f)
+        # If key already exists, it deletes the file
+        if key in hashMap.keys():
+            deletedFiles.append(f)
+            os.remove(f)  # Corrected line
+        else:
+            hashMap[key] = f
+    
+    if len(deletedFiles) != 0:
+        print('Deleted Files')
+        for i in deletedFiles:
+            print(i)
     else:
-        hashMap[key] = f
-if len(deletedFiles) != 0:
-    print('Deleted Files')
-    for i in deletedFiles:
-        print(i)
-else:
-    print('No duplicate files found')
+        print('No duplicate files found')
